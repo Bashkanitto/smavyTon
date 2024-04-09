323963f85f819b4e828bc78f8150200943a6859b
Nurzhigit <nurik.igisin@gmail.com>
2024-04-09 18:18:30 +0500

    locatstorage қостым

--- Main page/theme.js
+++ Main page/theme.js
@@ -1,6 +1,22 @@
 const checkbox = document.getElementById('checkbox');
 const body = document.body;
 
-checkbox.addEventListener('click', function(){
-    body.classList.toggle('dark')
-})
+let theme = localStorage.getItem("theme") || "light";
+
+if (theme === "dark") {
+  body.classList.add("dark");
+} else {
+  body.classList.remove("dark");
+}
+
+checkbox.addEventListener("click", function () {
+  if (theme == "dark") {
+    theme = "light";
+  } else {
+    theme = "dark";
+  }
+
+  localStorage.setItem("theme", theme);
+
+  body.classList.toggle("dark");
+});
